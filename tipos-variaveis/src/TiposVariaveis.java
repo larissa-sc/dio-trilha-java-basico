ac54fbbf7003e8b5fd12da1c9837e8d4207efd04
Larissa <larissa.cavalcante014@gmail.com>
2024-05-02 22:18:36 -0300

    treinamento inicial

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,17 @@
+public class TiposVariaveis{
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        //final vai determinar uma constante != variável
+        final double VALOR_PI = 3.14;
+
+        int numero = 5;
+
+        numero = - numero;
+
+        numero = -numero;
+
+        System.out.println(numero);
+        
+    }
+}
